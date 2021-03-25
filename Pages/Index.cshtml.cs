eba885b4d7584285ecdc20de5af5967be7871c5d
sansanee <sansaneea@gmail.com>
2021-03-25 19:39:00 -0400

    added rating

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using PagesMariaDB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PagesMariaDB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly StoreContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, StoreContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IList<Product> Products { get; set; }
+        public async Task OnGetAsync()
+        {
+            var products = from product in _context.Products select product;
+            Products = await products.ToListAsync();
+        }
+    }
+}
